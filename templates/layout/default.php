75a582a578ba85a4b2272abab0ecac8fe7486fc8
KimHak-mb <hak@mango-byte.com>
2023-08-30 16:42:46 +0700

    custom layout

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -86,7 +86,6 @@
     <div id="content">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-
                 <button type="button" id="sidebarCollapse" class="btn btn-color">
                     <!-- <i class="fas fa-align-left"></i> -->
                     <span>Toggle Sidebar</span>
